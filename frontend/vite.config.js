9d82b238a5ce2f50f497a9a8261e29d6cef10c78
ParagMalik23 <parag22csu129@ncuindia.edu>
2026-05-16 15:28:47 +0530

    main

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,14 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
+
   server: {
     proxy: {
       '/api': 'http://localhost:9000',
     },
   },
+
   preview: {
     host: '0.0.0.0',
-    allowedHosts: ['capable-enthusiasm-production.up.railway.app']
+    allowedHosts: ['team-task-manage.up.railway.app']
   }
 })
