d8eca233e97058e1f76a8d48fe58f93d2ace366e
ParagMalik23 <parag22csu129@ncuindia.edu>
2026-05-16 15:35:28 +0530

    main

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,6 +13,6 @@ export default defineConfig({
 
   preview: {
     host: '0.0.0.0',
-    allowedHosts: ['team-task-manage.up.railway.app']
+    allowedHosts: ['capable-enthusiasm-production.up.railway.app']
   }
 })
