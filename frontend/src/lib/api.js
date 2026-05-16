ec68d58ec4010def8e99e30fb48511633bd0879f
ParagMalik23 <parag22csu129@ncuindia.edu>
2026-05-16 13:43:52 +0530

    main

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,4 +1,4 @@
-const API_BASE = '/api'
+const API_BASE = import.meta.env.VITE_API_URL + '/api'
 
 export async function apiRequest(path, { token, method = 'GET', body } = {}) {
   const response = await fetch(`${API_BASE}${path}`, {
